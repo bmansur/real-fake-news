eee7a5696d8331712361063e1404b2842030ea5b
Bruno Mansur <bruno.mansur@gmail.com>
2021-01-26 09:26:27 -0300

    repository

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         </Widget>
         <Footer /> 
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/bmansur"/>
+      <GitHubCorner projectUrl="https://github.com/bmansur/real-fake-news"/>
     </QuizBackground>
   )
 }
